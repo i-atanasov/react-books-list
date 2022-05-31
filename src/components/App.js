95a1394d02631114b8129c7538b27de2c7098055
i-atanasov <ivayloatanasov@zohomail.eu>
2022-05-31 16:03:06 +0300

    Basic navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './Header';
 import BooksList from './BooksList';
 import BookAdd from './BookAdd';
 import BookEdit from './BookEdit';
+import BookDelete from './BookDelete';
+import history from '../history';
+
 
 const App = () => {
     return (
     <div>
-        <Header />
-        <BooksList />
-        <BookAdd />
-        <BookEdit />
+        <Router history={history}>
+            <Header />
+                <Routes>    
+                    <Route path='/' exact element={<BooksList />} />
+                    <Route path='/add' exact element={<BookAdd />} />
+                    <Route path='/edit' exact element={<BookEdit />} />
+                    <Route path='/delete' exact element={<BookDelete />} />
+                </Routes>
+        </Router>
     </div>
     );
 }
